57c67f163af58f9378a7cc9dc1c5120305d7e0a8
Shiho Yanagi <45202665+Yanagi-1989@users.noreply.github.com>
2021-03-29 20:22:13 +0900

    #37: PC用画面に、このサイトについて説明するページを追加

--- flask_app/static/page-switching.js
+++ flask_app/static/page-switching.js
@@ -2,10 +2,16 @@ const PageSwitchingApp = new Vue({
     el: '#left-navi',
     data: {
         isCalenderActive: true,
+        isAboutActive: false,
     },
     methods: {
         showCalender() {
             this.isCalenderActive = true;
+            this.isAboutActive = false;
+        },
+        showAbout() {
+            this.isCalenderActive = false;
+            this.isAboutActive = true;
         },
     },
 });
